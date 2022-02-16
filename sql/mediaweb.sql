20ee07810ec9cd6d4916bd30fa091374580f5db8
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-02-16 18:22:04 +0100

    Update mediaweb.sql

--- sql/mediaweb.sql
+++ sql/mediaweb.sql
@@ -23,15 +23,15 @@ INSERT INTO utilisateur VALUES (4, 'mahir', 'mahir', 'Mahir Akhayar', 0);
 INSERT INTO utilisateur VALUES (5, 'hassen', 'hassen', 'Hassen Arkab', 0);
 
 INSERT INTO document VALUES (0, 'Document 1');
-INSERT INTO document VALUES (0, 'Document 2');
-INSERT INTO document VALUES (0, 'Document 3');
-INSERT INTO document VALUES (0, 'Document 4');
-INSERT INTO document VALUES (0, 'Document 5');
-INSERT INTO document VALUES (0, 'Document 6');
-INSERT INTO document VALUES (0, 'Document 7');
-INSERT INTO document VALUES (0, 'Document 8');
-INSERT INTO document VALUES (0, 'Document 9');
-INSERT INTO document VALUES (0, 'Document 10');
+INSERT INTO document VALUES (1, 'Document 2');
+INSERT INTO document VALUES (2, 'Document 3');
+INSERT INTO document VALUES (3, 'Document 4');
+INSERT INTO document VALUES (4, 'Document 5');
+INSERT INTO document VALUES (5, 'Document 6');
+INSERT INTO document VALUES (6, 'Document 7');
+INSERT INTO document VALUES (7, 'Document 8');
+INSERT INTO document VALUES (8, 'Document 9');
+INSERT INTO document VALUES (9, 'Document 10');
 
 
 COMMIT;
