e456523fd07f14631a2d59448bfaee6b098b90e8
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-02-23 16:20:12 +0100

    Add document type (int) and rename stuff

--- sql/mediaweb.sql
+++ sql/mediaweb.sql
@@ -1,37 +1,38 @@
-CREATE TABLE utilisateur (
+CREATE TABLE user (
     userId INT,
     login VARCHAR(30),
     password VARCHAR(30),
     name VARCHAR(30),
-    isBibliothecaire BIT,
+    isLibrarian BIT,
     PRIMARY KEY (userId)
 );
 
 CREATE TABLE document (
     documentId INT,
     name VARCHAR(30),
+    doctype INT, -- 1: livre, 2: dvd, 3: cd
     PRIMARY KEY (documentId)
 );
 
 -- sample data
 
-INSERT INTO utilisateur VALUES (0, 'laurent', 'laurent', 'Laurent Han', 0);
-INSERT INTO utilisateur VALUES (1, 'ryan', 'ryan', 'Ryan Malonzo', 1);
-INSERT INTO utilisateur VALUES (2, 'fabien', 'fabien', 'Fabien Ribeiro', 1);
-INSERT INTO utilisateur VALUES (3, 'guillaume', 'guillaume', 'Guillaume Merchez', 1);
-INSERT INTO utilisateur VALUES (4, 'mahir', 'mahir', 'Mahir Akhayar', 0);
-INSERT INTO utilisateur VALUES (5, 'hassen', 'hassen', 'Hassen Arkab', 0);
+INSERT INTO user VALUES (0, 'laurent', 'laurent', 'Laurent Han', 0);
+INSERT INTO user VALUES (1, 'ryan', 'ryan', 'Ryan Malonzo', 1);
+INSERT INTO user VALUES (2, 'fabien', 'fabien', 'Fabien Ribeiro', 1);
+INSERT INTO user VALUES (3, 'guillaume', 'guillaume', 'Guillaume Merchez', 1);
+INSERT INTO user VALUES (4, 'mahir', 'mahir', 'Mahir Akhayar', 0);
+INSERT INTO user VALUES (5, 'hassen', 'hassen', 'Hassen Arkab', 0);
 
-INSERT INTO document VALUES (0, 'Document 1');
-INSERT INTO document VALUES (1, 'Document 2');
-INSERT INTO document VALUES (2, 'Document 3');
-INSERT INTO document VALUES (3, 'Document 4');
-INSERT INTO document VALUES (4, 'Document 5');
-INSERT INTO document VALUES (5, 'Document 6');
-INSERT INTO document VALUES (6, 'Document 7');
-INSERT INTO document VALUES (7, 'Document 8');
-INSERT INTO document VALUES (8, 'Document 9');
-INSERT INTO document VALUES (9, 'Document 10');
+INSERT INTO document VALUES (0, 'Livre 1', 1);
+INSERT INTO document VALUES (1, 'DVD 1', 2);
+INSERT INTO document VALUES (2, 'Livre 2', 1);
+INSERT INTO document VALUES (3, 'CD 1', 3);
+INSERT INTO document VALUES (4, 'DVD 2', 2);
+INSERT INTO document VALUES (5, 'Livre 3', 1);
+INSERT INTO document VALUES (6, 'Livre 4', 1);
+INSERT INTO document VALUES (7, 'CD 2', 3);
+INSERT INTO document VALUES (8, 'DVD 3', 2);
+INSERT INTO document VALUES (9, 'DVD 4', 2);
 
 
 COMMIT;
