1336b87417722b1e957d6a946ce79a1c60ce4ac9
Laurent HAN <laurenthan@hotmail.com>
2022-03-04 16:17:20 +0100

    Ajout Login

---
+++ src/services/Login.java
@@ -0,0 +1,30 @@
+package services;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import mediatek2022.*;
+import persistance.User;
+
+public class Login extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		HttpSession session = request.getSession(true);
+		String login = request.getParameter("pseudo");
+		String password = request.getParameter("motdepasse");
+		Utilisateur user = Mediatheque.getInstance().getUser(login, password);
+		
+		if(user != null) {
+			session.setAttribute("user", user);
+		}
+		else {
+			throw new IOException();
+		}
+		
+	}
+}
