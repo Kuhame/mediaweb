ff7876b256db4dfc20a2e08a0d806af45aecf5c6
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-13 22:09:43 +0100

    Make DB object static

--- src/persistance/DataBase.java
+++ src/persistance/DataBase.java
@@ -12,10 +12,39 @@ public class DataBase {
 	private static final String url_jdbc = "jdbc:mysql://localhost/mediaweb";
 	private static final String login = "root";
 	private static final String password = "";
+	private static Connection connection;
 
+	static {
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+		System.out.println("JE SUIS UNE GROSSE SALOPE");
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection =  DriverManager.getConnection(url_jdbc, login, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private DataBase() {}
 	
-	public static Connection connexionBD() throws Exception {
-		Class.forName("com.mysql.jdbc.Driver");
-		return DriverManager.getConnection(url_jdbc, login, password);
+	public static Connection connexionBD() {
+		return connection;
 	}
 }
